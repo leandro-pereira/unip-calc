5d211c370614641dcb0ca369ff053cb6ad56a226
Leandro Pereira <leandro-pereira2011@hotmail.com>
2019-09-30 15:18:41 -0300

    Atulizando o state, convertendo e mostrando o calculo

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,20 +1,77 @@
-import React, {Component } from 'react'
-import { Text, TextInput, View, StyleSheet, StatusBar} from 'react-native'
+import React, { Component } from 'react'
+import { ImageBackground, View, Text, TextInput, Button, StyleSheet, StatusBar, Image } from 'react-native'
+import GradeInput from './componentes/GradeInput'
 
-export default class Calculadora extends Component{
-    render(){
-        return(
-            <View style={estilo.container}>
-            <StatusBar backgroundColor="#263a7f" barStyle="light-content"/>
-            <Text style={estilo.format}>Nota P1</Text>
-            <TextInput style={estilo.textfield} keyboardType={'numeric'}  maxLength={4}  ></TextInput>
-            <Text style={estilo.format}>Nota P2</Text>
-            <TextInput style={estilo.textfield} keyboardType={'decimal-pad'} maxLength={4}></TextInput>
-            <Text style={estilo.format}>Sua média é:</Text>
-            <Text style={estilo.format}>Exame</Text>
-            <TextInput style={estilo.textfield} keyboardType={'decimal-pad'} maxLength={4}></TextInput>
-            <Text style={estilo.format}>Média mais exame :</Text>
-            </View>
+
+export default class Calculadora extends Component {
+
+
+    state = {
+        grade1: 0,
+        grade2: 0,
+        exame: 0
+    }
+
+
+    handleNota1 = texto => {
+        console.warn({})
+    }
+
+    handleNota2 = grade2 => {
+        this.setState({ grade2 })
+        console.warn({ handleNota1, grade2 })
+    }
+
+    handleExame = exame => {
+        this.setState({ exame })
+        console.warn({ exame })
+    }
+
+    calcular() {
+        const p1 = this.state.grade1
+        const p2 = this.state.grade2
+        const ex = this.state.exame
+        console.warn({ p1, p2, ex })
+    }
+
+    isApproved = () => {
+        const { nota1, nota2 } = this.state
+        const media = (nota1 + nota2) / 2
+        return media >= 7
+    }
+
+    render() {
+
+        const average = this.isApproved();
+        const texto = average ? "Aprovado" : "Exame"
+
+        return (
+            <ImageBackground source={require('./img/wallp.jpg')} style={estilo.container}>
+                <StatusBar backgroundColor="#263a7f" barStyle="light-content" />
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5 }}>
+                    <Button title="Menu" color='red' onPress={() => this.props.navigation.openDrawer()} />
+                    <Image source={require('./img/logo-site.png')} style={{ width: 160, height: 50 }} />
+                </View>
+                <View style={{ alignItems: 'center', margin: 100 }}>
+                    <Text style={estilo.label}>Nota 1</Text>
+                    <TextInput
+                        style={estilo.gradeInput}
+                        keyboardType={'numeric'}
+                        maxLength={4}
+                        onChangeText={(text) => this.setState({ grade1: text })}
+                        value={this.state.grade1}
+                    />
+                    <Text style={estilo.label}>Nota 2</Text>
+                    <TextInput
+                        style={estilo.gradeInput}
+                        keyboardType={'numeric'}
+                        maxLength={4}
+                        onChangeText={(text) => this.setState({ grade2: text })}
+                        value={this.state.grade2}
+                    />
+                    <Text style={estilo.label}>Nota :{p1 = parseFloat(this.state.grade1), p2 = parseFloat(this.state.grade2), (p1 + p2) / 2}</Text>
+                </View>
+            </ImageBackground>
         )
     }
 }
@@ -22,15 +79,26 @@ export default class Calculadora extends Component{
 const estilo = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'flex-start',
         backgroundColor: "#5595ff"
     },
-    format:{
+    format: {
         fontSize: 30,
         textAlign: "center"
     },
-    textfield:{
+    textfield: {
+        fontSize: 20,
+        height: 50,
+        width: 100,
+        textAlign: "center",
+        borderRadius: 10,
+        backgroundColor: 'grey'
+    },
+    label: {
+        fontSize: 30,
+        textAlign: "center"
+    },
+    gradeInput: {
         fontSize: 20,
         height: 50,
         width: 100,
--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { createDrawerNavigator } from "react-navigation"
-import  Simples  from './componentes/Simples'
+import Simples from './componentes/Simples'
 import Calculadora from './Calculadora'
 
 export default createDrawerNavigator({
-    Calculadora:{
-        screen: () => <Calculadora></Calculadora>
-    },
+    Calculadora: Calculadora,
 
     Simples: {
         screen: () => <Simples texto='Flexivel' />
     }
-}, {drawerWidth: 150 })
+}, { drawerWidth: 150 })
 
 
